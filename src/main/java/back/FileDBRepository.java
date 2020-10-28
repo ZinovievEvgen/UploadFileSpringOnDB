5f0f37521fff66033d652a0dd39d291ed8ad2d9d
ZinovievEvgen <natsy29@mail.ru>
2020-10-28 20:42:15 +0400

    init commit

---
+++ src/main/java/back/FileDBRepository.java
@@ -0,0 +1,10 @@
+package back;
+
+
+import back.models.FileDB;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FileDBRepository extends JpaRepository<FileDB, String> {
+}
